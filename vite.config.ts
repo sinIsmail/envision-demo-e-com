abec8446b01f8f43069c40647e6e81f3f3dcd735
sinIsmail <devmdismail@gmail.com>
2026-09-16 23:05:09 +0530

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production"
-    ? "/envision-demo-com/"
-    : "/",
+  base: "/envision-demo-com/",
 
   plugins: [react(), tailwindcss()],
 
