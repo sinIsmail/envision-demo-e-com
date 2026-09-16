4b5742f51c270228294691e21481c485039b9e55
sinIsmail <devmdismail@gmail.com>
2026-09-16 22:50:37 +0530

    first commit 5

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/envision-demo-com/",
+  base: "/",
 
   plugins: [react(), tailwindcss()],
 
